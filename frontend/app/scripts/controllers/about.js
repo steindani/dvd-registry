133cf628948ffd364623c86dfd2c338d31f8f031
Dániel Stein <daniel@stein.hu>
2015-10-15 14:40:27 +0200

    Frontend init

---
+++ frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dvdApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dvdApp
+ */
+angular.module('dvdApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dvdApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dvdApp
+ */
+angular.module('dvdApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
