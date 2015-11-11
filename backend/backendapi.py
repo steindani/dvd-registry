70b9747974b08ff2ed630575fe917e78379b818f
benjo <benedekh12@gmail.com>
2015-11-11 09:54:34 +0100

    query moviebase from db

--- backend/backendapi.py
+++ backend/backendapi.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, jsonify
 from db.dbmanager import DBManager
 from tmdb.tmdbhelper import TMDBHelper
+from db.entities.base import Base
+from db.entities.genre import Genre
+from db.entities.medium import Medium
+from db.entities.movie import MovieBase, MovieExtra
+from db.entities.ownershiptriplet import OwnershipTriplet
+from db.entities.person import Person
+from db.entities.user import User
 
 app = Flask( __name__ )
 
@@ -8,6 +15,30 @@ dbc = DBManager()
 dbc.init_db()
 
 tmdb = TMDBHelper()
+
+
+# # DEBUG
+medium = Medium( name = "Hello" )
+person = Person( name = "John Doe" )
+user = User( googleid = 12 )
+genre = Genre( name = "Action" )
+moviebase = MovieBase( title = "Bronze", cover = "http://waaa" )
+movieextra = MovieExtra( year = 1923, plot = "BLALALALAAL", trailer = "http://woo" )
+
+moviebase.tmdb_id = 1
+moviebase.extra = movieextra
+
+movieextra.genres.append( genre )
+movieextra.cast.append( person )
+
+dbc.add_movie( moviebase )
+
+medium.user = user
+dbc.add_medium( medium )
+
+ownertrip = OwnershipTriplet( user, moviebase, medium )
+dbc.add_ownertriplet( ownertrip )
+# # DEBUG
 
 @app.route( '/login', methods = ['POST'] )
 def login():
@@ -70,7 +101,11 @@ def get_movies():
         ] 
     } 
     '''
-    pass
+    
+    googleid = str( request.cookies.get( 'googleid' ) )
+    user = dbc.get_user_by_googleid( googleid )
+    result = [{"movie_id": triple.movie.id, "title": triple.movie.title, "cover": triple.movie.cover} for triple in user.triplet]
+    return jsonify( movies = result )
 
 @app.route( '/movie/<int:movie_id>', methods = ['GET'] )
 def get_movie( movie_id ):
