c89b37b768f22e7d232852d3fc0e3fb7e743533c
benjo <benedekh12@gmail.com>
2015-11-17 11:42:50 +0100

    translation helper from en to hu using yandex instaed of google

---
+++ backend/translate/yandexhelper.py
@@ -0,0 +1,19 @@
+'''
+Created on Nov 17, 2015
+
+@author: benjo
+'''
+
+import requests, json
+
+api_key = 'trnsl.1.1.20151117T102755Z.bfcf9daea0a5eec5.05eead1277a72db8492c639285107c946c074188'
+yandex_prefix = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + api_key
+add_text = '&text='
+yandex_postfix = '&lang=en-hu'
+
+def translate( text ):
+    response = requests.request( 'GET', yandex_prefix + add_text + str( text ) + yandex_postfix )
+    data = json.loads( response.text )
+    return data['text'][0]
+    
+print( translate( 'That\'s not portable, as they made an API breaking change in requests v1 (json is a method rather than an attribute now), and Debian\'s current stable version (wheezy) is still using v0. (Although v2 is now available in backports.) ' ) )
