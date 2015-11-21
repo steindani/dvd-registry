2f18f9a782599dd9e8b4f28c6177e5470167b691
benjo <benedekh12@gmail.com>
2015-11-21 21:34:18 +0100

    api keys from os.getenv as well

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,5 +1,7 @@
-TOKEN_SECRET = 'JWT Token Secret String'
-GOOGLE_SECRET = 'FnK9Fh3Oq9HIlvLYxFiBcgAs'
-TMDB_KEY = '13ed7e5e07699386ba2c32a52aed7ae6'
-YOUTUBE_KEY = 'AIzaSyDZSAWLGft1AxX-U4Dunw2SfStcHujU4nw'
-YANDEX_KEY = 'trnsl.1.1.20151117T102755Z.bfcf9daea0a5eec5.05eead1277a72db8492c639285107c946c074188'
+import os
+
+TOKEN_SECRET = os.environ.get( 'TOKEN_SECRET' ) or 'JWT Token Secret String' 
+GOOGLE_SECRET = os.environ.get( 'GOOGLE_SECRET' ) or 'FnK9Fh3Oq9HIlvLYxFiBcgAs'
+TMDB_KEY = os.environ.get( 'TMDB_KEY' ) or '13ed7e5e07699386ba2c32a52aed7ae6'
+YOUTUBE_KEY = os.environ.get( 'YOUTUBE_KEY' ) or  'AIzaSyDZSAWLGft1AxX-U4Dunw2SfStcHujU4nw'
+YANDEX_KEY = os.environ.get( 'YANDEX_KEY' ) or 'trnsl.1.1.20151117T102755Z.bfcf9daea0a5eec5.05eead1277a72db8492c639285107c946c074188'
