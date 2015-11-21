9a134888da49a053f93ab4643a01ad5cbca9dedb
Dániel Stein <daniel@stein.hu>
2015-11-21 12:14:04 +0100

    Details and login button

--- frontend/app/scripts/controllers/LoginController.ts
+++ frontend/app/scripts/controllers/LoginController.ts
@@ -12,22 +12,33 @@ module dvdApp.Controllers {
             private $location: ng.ILocationService
         ) {
             this.scope = $scope;
-            
+
             $scope.authenticate = function(provider) {
                 $auth.authenticate(provider)
-                .then((response) => {
-                    $auth.setToken(response.data.token);
-                    $location.url("/");
-                });
+                    .then((response) => {
+                        $auth.setToken(response.data.token);
+                        $location.url("/");
+                    });
             };
+
+            $scope.isAuthenticated = () => {
+                return $auth.isAuthenticated();
+            }
+
+            $scope.logOut = () => {
+                $auth.removeToken();
+                $location.url("/");
+            }
         }
     }
 
     angular
         .module('dvdApp.Controllers', [])
         .controller('LoginController', ['$scope', '$auth', '$location', dvdApp.Controllers.LoginController]);
-        
+
     export interface ILoginScope extends ng.IScope {
         authenticate: (provider: string) => void;
+        isAuthenticated: () => boolean;
+        logOut: () => void;
     }
 }
