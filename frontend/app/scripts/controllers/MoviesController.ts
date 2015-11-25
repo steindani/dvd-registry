f08173fdd010a5307c37154e632bd2b44fcf7f7f
Dániel Stein <daniel@stein.hu>
2015-11-25 11:56:49 +0100

    Filter and order working, design fixes

--- frontend/app/scripts/controllers/MoviesController.ts
+++ frontend/app/scripts/controllers/MoviesController.ts
@@ -42,6 +42,8 @@ module dvdApp.Controllers {
                     plain: false,
                     scope: childScope
                 });
+
+                updateRecommendations();
             }
 
             $scope.newMovieModal = function() {
